67d1e48ff10ccaf8fb26672cd8d359d25ea257a8
cmatthews <calebjmatthews@gmail.com>
2016-06-13 16:22:36 -0500

    Add and populate Climate class
     Changes to be committed:
    	modified:   app/static/js/main.ts

--- app/static/js/main.ts
+++ app/static/js/main.ts
@@ -68,7 +68,7 @@ enum eSIZE { Small, Medium, Large, Gigantic }
 enum eSHAPE { Round, Bay, Twin, Jagged, Thin }
 
 // Enumerates land climate options
-enum eCLIMATE { Grassy, Forested, Rocky, Desert, Varied }
+enum eCLIMATE { Grassy, Forested, Rocky, Desert, Varied, Jungle, Wet, Mountain }
 
 // Enumerates landscape types for individual tiles
 enum eLSCP { Grassy, Shore, Forested, Rocky, Desert, Sea }
@@ -387,6 +387,74 @@ class Land {
 		renderer.render(stage);
 	}
 }
+
+class Climate {
+	id: number;
+	devel: number; // The probability of randgen black developments
+	prob: number[]; // An array of probabilities for the landscape types
+
+	constructor(setID: number, setDevel: number;) {
+		this.id = setID;
+		this.devel = setDevel;
+	}
+}
+
+var climateArray = [];
+climateArray[eCLIMATE.Desert] = new Climate(eCLIMATE.Desert, 0.1);
+climateArray[eCLIMATE.Desert].prob = [];
+climateArray[eCLIMATE.Desert].prob[eLSCP.Desert] = 0.3;
+climateArray[eCLIMATE.Desert].prob[eLSCP.Forested] = 0.05;
+climateArray[eCLIMATE.Desert].prob[eLSCP.Grassy] = 0.15;
+climateArray[eCLIMATE.Desert].prob[eLSCP.Rocky] = 0.1;
+
+climateArray[eCLIMATE.Forested] = new Climate(eCLIMATE.Forested, 0.1);
+climateArray[eCLIMATE.Forested].prob = [];
+climateArray[eCLIMATE.Forested].prob[eLSCP.Desert] = 0.05;
+climateArray[eCLIMATE.Forested].prob[eLSCP.Forested] = 0.6;
+climateArray[eCLIMATE.Forested].prob[eLSCP.Grassy] = 0.2;
+climateArray[eCLIMATE.Forested].prob[eLSCP.Rocky] = 0.1;
+
+climateArray[eCLIMATE.Grassy] = new Climate(eCLIMATE.Grassy, 0.1);
+climateArray[eCLIMATE.Grassy].prob = [];
+climateArray[eCLIMATE.Grassy].prob[eLSCP.Desert] = 0.1;
+climateArray[eCLIMATE.Grassy].prob[eLSCP.Forested] = 0.2;
+climateArray[eCLIMATE.Grassy].prob[eLSCP.Grassy] = 0.4;
+climateArray[eCLIMATE.Grassy].prob[eLSCP.Rocky] = 0.1;
+
+climateArray[eCLIMATE.Rocky] = new Climate(eCLIMATE.Rocky, 0.1);
+climateArray[eCLIMATE.Rocky].prob = [];
+climateArray[eCLIMATE.Rocky].prob[eLSCP.Desert] = 0.1;
+climateArray[eCLIMATE.Rocky].prob[eLSCP.Forested] = 0.1;
+climateArray[eCLIMATE.Rocky].prob[eLSCP.Grassy] = 0.2;
+climateArray[eCLIMATE.Rocky].prob[eLSCP.Rocky] = 0.4;
+
+climateArray[eCLIMATE.Varied] = new Climate(eCLIMATE.Varied, 0.2);
+climateArray[eCLIMATE.Varied].prob = [];
+climateArray[eCLIMATE.Varied].prob[eLSCP.Desert] = 0.2;
+climateArray[eCLIMATE.Varied].prob[eLSCP.Forested] = 0.4;
+climateArray[eCLIMATE.Varied].prob[eLSCP.Grassy] = 0.1;
+climateArray[eCLIMATE.Varied].prob[eLSCP.Rocky] = 0.4;
+
+climateArray[eCLIMATE.Jungle] = new Climate(eCLIMATE.Jungle, 0.5);
+climateArray[eCLIMATE.Jungle].prob = [];
+climateArray[eCLIMATE.Jungle].prob[eLSCP.Desert] = 0.05;
+climateArray[eCLIMATE.Jungle].prob[eLSCP.Forested] = 0.8;
+climateArray[eCLIMATE.Jungle].prob[eLSCP.Grassy] = 0.2;
+climateArray[eCLIMATE.Jungle].prob[eLSCP.Rocky] = 0.1;
+
+climateArray[eCLIMATE.Mountain] = new Climate(eCLIMATE.Mountain, 0.5);
+climateArray[eCLIMATE.Mountain].prob = [];
+climateArray[eCLIMATE.Mountain].prob[eLSCP.Desert] = 0.1;
+climateArray[eCLIMATE.Mountain].prob[eLSCP.Forested] = 0.1;
+climateArray[eCLIMATE.Mountain].prob[eLSCP.Grassy] = 0.2;
+climateArray[eCLIMATE.Mountain].prob[eLSCP.Rocky] = 0.6;
+
+climateArray[eCLIMATE.Wet] = new Climate(eCLIMATE.Wet, 0.5);
+climateArray[eCLIMATE.Wet].prob = [];
+climateArray[eCLIMATE.Wet].prob[eLSCP.Desert] = 0.1;
+climateArray[eCLIMATE.Wet].prob[eLSCP.Forested] = 0.2;
+climateArray[eCLIMATE.Wet].prob[eLSCP.Grassy] = 0.4;
+climateArray[eCLIMATE.Wet].prob[eLSCP.Rocky] = 0.1;
 
 class Landcape {
 	id: number;
